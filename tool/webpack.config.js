206335cb28228d1b933b7723e8c5c5db55b2aef3
kaiduo <kaiduo@pinduoduo.com>
2021-02-01 00:22:32 +0800

    feat: 升级依赖

--- tool/webpack.config.js
+++ tool/webpack.config.js
@@ -1,8 +1,14 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 exports.__esModule = true;
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require("path");
 var hotMiddlewareScript = "webpack-hot-middleware/client?reload=true";
 var isProduction = process.env.NODE_ENV === "production";
@@ -15,15 +21,9 @@ console.log("WebPack build in " + process.env.NODE_ENV + " ...");
 var proPlugins = [
     new webpack.DefinePlugin({
         "process.env": {
-            "NODE_ENV": JSON.stringify("production")
+            NODE_ENV: JSON.stringify("production")
         }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false,
-            drop_console: true
-        }
-    })
 ];
 var devPlugins = [
     new webpack.DefinePlugin({
@@ -31,11 +31,11 @@ var devPlugins = [
             NODE_ENV: JSON.stringify("development")
         }
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
 ];
 var currentPlugins = isProduction ? proPlugins : devPlugins;
 var config = {
-    entry: commonsEntry.concat([joinDir("../src/app.tsx")]),
+    entry: __spreadArrays(commonsEntry, [joinDir("../src/app.tsx")]),
     output: {
         path: joinDir("../dist"),
         publicPath: "./",
@@ -57,8 +57,7 @@ var config = {
             context: joinDir("../dist/build"),
             manifest: require("../dist/build/bundle.manifest.json")
         }),
-        new ExtractTextPlugin("css/[name].css"),
-        new webpack.NoEmitOnErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
     ]),
     module: {
         rules: [
@@ -75,20 +74,17 @@ var config = {
                 }
             },
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "sass-loader"],
-                    publicPath: "../"
-                })
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: "url-loader?limit=10000&name=images/[hash:8].[name].[ext]"
-            }, {
+            },
+            {
                 test: /\.html$/,
                 loader: "html-loader"
-            }
+            },
         ]
     }
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,5 @@
 import * as webpack from "webpack";
 import * as HtmlWebpackPlugin from "html-webpack-plugin";
-import * as ExtractTextPlugin from "extract-text-webpack-plugin";
 import * as path from "path";
 
 const hotMiddlewareScript = "webpack-hot-middleware/client?reload=true";
@@ -8,34 +7,28 @@ const hotMiddlewareScript = "webpack-hot-middleware/client?reload=true";
 const isProduction: boolean = process.env.NODE_ENV === "production";
 const commonsEntry: string[] = [];
 
-if ( !isProduction ) {
+if (!isProduction) {
   commonsEntry.unshift(hotMiddlewareScript);
 }
 
-const joinDir = p => path.join(__dirname, p);
+const joinDir = (p) => path.join(__dirname, p);
 
 console.log(`WebPack build in ${process.env.NODE_ENV} ...`);
 
 const proPlugins: webpack.ResolvePlugin[] = [
   new webpack.DefinePlugin({
     "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
+      NODE_ENV: JSON.stringify("production"),
+    },
   }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false,
-      drop_console: true
-    }
-  })
 ];
 const devPlugins: webpack.ResolvePlugin[] = [
   new webpack.DefinePlugin({
     "process.env": {
-      NODE_ENV: JSON.stringify("development")
-    }
+      NODE_ENV: JSON.stringify("development"),
+    },
   }),
-  new webpack.HotModuleReplacementPlugin()
+  new webpack.HotModuleReplacementPlugin(),
 ];
 const currentPlugins = isProduction ? proPlugins : devPlugins;
 
@@ -45,58 +38,53 @@ const config: webpack.Configuration = {
     path: joinDir("../dist"),
     publicPath: "./",
     filename: "js/[name].[hash].js",
-    chunkFilename: "js/[name].[hash].chunk.js"
+    chunkFilename: "js/[name].[hash].chunk.js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".scss"],
     alias: {
-      common: joinDir("../src/common")
-    }
+      common: joinDir("../src/common"),
+    },
   },
   plugins: currentPlugins.concat([
     new HtmlWebpackPlugin({
-        template: joinDir("../src/index.html"),
-        filename: "index.html"
+      template: joinDir("../src/index.html"),
+      filename: "index.html",
     }),
     new webpack.DllReferencePlugin({
       context: joinDir("../dist/build"),
       manifest: require("../dist/build/bundle.manifest.json"),
     }),
-    new ExtractTextPlugin("css/[name].css"),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ]),
   module: {
     rules: [
-        {
-            test: /\.(tsx|ts)$/,
-            use: "ts-loader"
+      {
+        test: /\.(tsx|ts)$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"],
         },
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            query:
-            {
-              presets: ["es2015", "react"]
-            }
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: ["css-loader", "sass-loader"],
-            publicPath: "../"
-          })
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          use: "url-loader?limit=10000&name=images/[hash:8].[name].[ext]"
-        }, {
-          test: /\.html$/,
-          loader: "html-loader"
-        }
-    ]
-  }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: "url-loader?limit=10000&name=images/[hash:8].[name].[ext]",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
 };
 
 export default config;
